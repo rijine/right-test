1d98d63c342d915596a9fd183d15aa12759b07fd
Rijin Elayambari <e.rijin@gmail.com>
2023-10-29 00:11:42 +0100

    fix: handling exceptions in api calls.

--- src/api/baseApi.js
+++ src/api/baseApi.js
@@ -3,6 +3,9 @@ export const BASE_URL = 'http://localhost:3000/api';
 export const getApi = async (url) => {
     try {
         const response = await fetch(`${BASE_URL}/${url}`);
+        if (!response.ok) {
+            return { message: response.statusText, success: false };
+        }
         const data = await response.json();
         return { data, success: true };
     } catch (error) {
